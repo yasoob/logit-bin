f9145b6bb8fde895d5ae8fa2f18f50a5de4acd55
M.Yasoob Ullah Khalid ☺ <yasoob.khld@gmail.com>
2013-12-28 23:17:16 +0500

    added flask_wtf and debug = False

--- app.py
+++ app.py
@@ -204,4 +204,4 @@ def logout():
 #-----------------------------------------------------------------------------------------------#
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port, debug=True)
+    app.run(host='0.0.0.0', port=port, debug=False)
