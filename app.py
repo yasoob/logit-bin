1fe4e0d5424acc89c521feef866c6ecabbcb30af
yasoob <yasoob.khld@gmail.com>
2014-02-16 01:05:47 +0500

    changed debug to False

--- app.py
+++ app.py
@@ -315,4 +315,4 @@ def logout():
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port, debug=True)
+    app.run(host='0.0.0.0', port=port, debug=False)
