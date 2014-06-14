73df07187f04228be46baa232a1b0b5a4a234146
Nauman Ahmad <nauman-ahmad@outlook.com>
2014-06-14 20:13:38 +0500

    PEP8 Issues Fixed
    Lot’s of issues has been fixed :smiley:

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask, request, url_for, redirect, g, session, flash, abort, render_template, make_response,send_from_directory
-from forms import LoginForm , SignupForm
+from flask import Flask, request, url_for, redirect, g, session, flash, \
+abort, render_template, make_response
+from forms import LoginForm, SignupForm
 from flask.ext.sqlalchemy import SQLAlchemy
 from datetime import datetime
 import os
@@ -8,7 +9,7 @@ import difflib
 import uuid
 
 app = Flask(__name__)
-app.config['SECRET_KEY']= 'ANYTHING SECRET'
+app.config['SECRET_KEY'] = os.urandom(24)
 
 if os.environ.get('DATABASE_URL') is None:
     app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///paste.db"
@@ -16,11 +17,9 @@ else:
     app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 
 db = SQLAlchemy(app)
-#-----------------------------------------------------------------------------------------------#
-#                                                Models                                         #
-#-----------------------------------------------------------------------------------------------#
 
-class Comment(db.Model): 
+
+class Comment(db.Model):
     __tablename__ = "comment"
     id = db.Column(db.Integer, primary_key=True)
     message = db.Column(db.Text)
@@ -28,11 +27,11 @@ class Comment(db.Model):
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     parent_paste = db.Column(db.Integer, db.ForeignKey('paste.id'))
 
-    def __init__(self,message,user,paste):
+    def __init__(self, message, user, paste):
         self.message = message
         self.pub_date = datetime.utcnow()
         self.user = user
-        self.paste = paste 
+        self.paste = paste
 
     def __repr__(self):
         return '<Comment %r>' % self.id
@@ -45,16 +44,17 @@ class User(db.Model):
     password = db.Column(db.String(120))
     email = db.Column(db.String(240))
     pastes = db.relationship('Paste', lazy='dynamic', backref='user')
-    comments = db.relationship('Comment', lazy='dynamic', backref='user', cascade="all, delete-orphan")
+    comments = db.relationship(
+        'Comment', lazy='dynamic', backref='user', cascade="all, delete-orphan")
 
     def __init__(self, name, email, password):
         self.name = name
         self.email = email
         self.set_password(password)
-    
+
     def set_password(self, password):
         self.password = generate_password_hash(password)
-       
+
     def check_password(self, password):
         return check_password_hash(self.password, password)
 
@@ -71,10 +71,11 @@ class Paste(db.Model):
     anonymous = db.Column(db.Boolean, unique=False, default=False)
     pub_date = db.Column(db.DateTime)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-    reply_to = db.Column(db.Integer,default=None)
-    comments = db.relationship('Comment', lazy='dynamic', cascade="all, delete-orphan", backref='paste')
+    reply_to = db.Column(db.Integer, default=None)
+    comments = db.relationship(
+        'Comment', lazy='dynamic', cascade="all, delete-orphan", backref='paste')
 
-    def __init__(self, user, title, code,anonymous = False,reply_to=None):
+    def __init__(self, user, title, code, anonymous=False, reply_to=None):
         self.user = user
         self.title = title
         self.anonymous = anonymous
@@ -87,31 +88,24 @@ class Paste(db.Model):
         return '<Paste %r>' % self.id
 
 
-#-----------------------------------------------------------------------------------------------#
-#                                            Before Request                                     #
-#-----------------------------------------------------------------------------------------------#
-
 @app.before_request
 def check_user_status():
     g.user = None
     if 'user_email' in session:
-        g.user = User.query.filter_by(email = session['user_email']).first()
+        g.user = User.query.filter_by(email=session['user_email']).first()
     else:
         session['user_email'] = None
         session['user_name'] = None
 
-#-----------------------------------------------------------------------------------------------#
-#                                                Routes                                         #
-#-----------------------------------------------------------------------------------------------#
 
 @app.route('/', methods=('GET', 'POST'))
 def home():
     reply_to = None
     parent = None
-    reply_to_title=''
+    reply_to_title = ''
     reply_to = None
     if session['user_name']:
-        user = User.query.filter_by(name = session['user_name']).first()
+        user = User.query.filter_by(name=session['user_name']).first()
         if user.pastes.all():
             recent_paste = user.pastes.all()[-1]
         else:
@@ -121,13 +115,14 @@ def home():
     if request.method == 'POST' and request.form['code']:
         visibility = True
         if request.form['reply_to']:
-            reply_to= request.form['reply_to']
+            reply_to = request.form['reply_to']
         if request.form['visibility'] == "secret":
             visibility = False
         title = None
         if request.form['title']:
             title = request.form['title']
-        paste = Paste(g.user,title,request.form['code'],visibility,reply_to)
+        paste = Paste(
+            g.user, title, request.form['code'], visibility, reply_to)
         db.session.add(paste)
         db.session.commit()
         return redirect(url_for('show_paste', paste_id=paste.uuid_id))
@@ -135,50 +130,66 @@ def home():
         flash('You need to fill in the code field')
     if request.method == 'GET' and request.args.get('reply_to'):
         reply_id = request.args.get('reply_to')
-        paste = Paste.query.filter_by(id = reply_id).first()
+        paste = Paste.query.filter_by(id=reply_id).first()
         if paste is not None:
-            reply_to_title = "Reply to #%s"%(paste.id)
-            return render_template('home.html', parent=parent, recent_paste=recent_paste,reply_to = paste,reply_to_title=reply_to_title)    
-    return render_template('home.html', parent=parent, recent_paste=recent_paste,reply_to=reply_to,reply_to_title=reply_to_title)
+            reply_to_title = "Reply to #%s" % (paste.id)
+            return render_template('home.html',
+                                    parent=parent,
+                                    recent_paste=recent_paste,
+                                    reply_to=paste,
+                                    reply_to_title=reply_to_title)
+    return render_template('home.html',
+                            parent=parent,
+                            recent_paste=recent_paste,
+                            reply_to=reply_to,
+                            reply_to_title=reply_to_title)
 
-@app.route('/<paste_id>',methods=('GET','POST'))
+
+@app.route('/<paste_id>', methods=('GET', 'POST'))
 def show_paste(paste_id):
-    if request.method == 'POST' and request.form['comment'] and session['user_name']:
-        paste = Paste.query.filter_by(uuid_id = paste_id).first()
-        user = User.query.filter_by(name = session['user_name']).first()
-        comment = Comment(request.form['comment'],user,paste)
+    if request.method == 'POST' and request.form['comment'] and \
+    session['user_name']:
+        paste = Paste.query.filter_by(uuid_id=paste_id).first()
+        user = User.query.filter_by(name=session['user_name']).first()
+        comment = Comment(request.form['comment'], user, paste)
         db.session.add(comment)
         db.session.commit()
         flash('comment was successful')
         if paste:
-            return render_template('show_paste.html', paste = paste)
+            return render_template('show_paste.html', paste=paste)
         else:
-            flash('You tried to post comment on a paste which is not available')
+            flash(
+                'You tried to post comment on a paste which is not available')
             return redirect(request.referrer or url_for())
     elif request.method == 'POST' and request.form['comment']:
         flash('In order to comment you need to be logged in!')
-        return render_template('show_paste.html', paste = paste)
-    paste = Paste.query.filter_by(uuid_id = paste_id).first()
-    if paste: 
-        return render_template('show_paste.html', paste = paste)
+        return render_template('show_paste.html', paste=paste)
+    paste = Paste.query.filter_by(uuid_id=paste_id).first()
+    if paste:
+        return render_template('show_paste.html', paste=paste)
     else:
         abort(404)
 
+
 @app.route('/<paste_id>/raw')
 def view_raw(paste_id):
-    response = make_response(Paste.query.filter_by(uuid_id = paste_id).first().code)
+    response = make_response(
+        Paste.query.filter_by(uuid_id=paste_id).first().code)
     response.headers["content-type"] = "text/plain"
     return response
+
 
 @app.route('/<paste_id>/delete', methods=['GET', 'POST'])
 def delete_paste(paste_id):
     paste = Paste.query.get(paste_id)
     if paste.user:
-        if session['user_name'] is None or session['user_name'] != paste.user.name:
+        if session['user_name'] is None or session['user_name'] != \
+        paste.user.name:
             abort(401)
     if request.method == 'POST':
-        if 'yes' in request.form and session['user_name'] is not None and session['user_name'] == paste.user.name:
-            comments = paste.user.comments.filter_by(parent_paste = 1).all()
+        if 'yes' in request.form and session['user_name'] is not None \
+        and session['user_name'] == paste.user.name:
+            comments = paste.user.comments.filter_by(parent_paste=1).all()
             db.session.delete(paste)
             db.session.commit()
             flash('Paste was successfully deleted')
@@ -187,76 +198,89 @@ def delete_paste(paste_id):
             return redirect(url_for('show_paste', paste_id=paste.uuid_id))
     return render_template('delete_paste.html', paste=paste)
 
+
 @app.route('/diff')
 def show_diff():
     paste1 = request.args.get('paste1')
     paste2 = request.args.get('paste2')
     paste1 = Paste.query.get_or_404(paste1)
     paste2 = Paste.query.get_or_404(paste2)
-    diff = difflib.unified_diff(paste1.code.splitlines(),paste2.code.splitlines(),fromfile="paste #"+str(paste1.id),tofile="paste #"+str(paste2.id))
+    diff = difflib.unified_diff(paste1.code.splitlines(), paste2.code.splitlines(
+    ), fromfile="paste #" + str(paste1.id), tofile="paste #" + str(paste2.id))
     diff_list = []
     try:
         while 1:
             diff_list.append(''.join(diff.next()))
     except:
         pass
-    return render_template('show_diff.html',diff=diff_list,to=paste2,frompaste=paste1)
+    return render_template('show_diff.html', diff=diff_list,
+                           to=paste2, frompaste=paste1)
 
 
 @app.route('/my_pastes', methods=('GET', 'POST'))
 def my_pastes():
     if request.method == 'GET':
         if session['user_email'] and session['user_name']:
-            user_pastes = User.query.filter_by(name = session['user_name']).first().pastes.all()
-            user_comments = User.query.filter_by(name = session['user_name']).first().comments.all()
-            return render_template('my_pastes.html',pastes = user_pastes,comments = user_comments)
+            user_pastes = User.query.filter_by(
+                name=session['user_name']).first().pastes.all()
+            user_comments = User.query.filter_by(
+                name=session['user_name']).first().comments.all()
+            return render_template('my_pastes.html',
+                                    pastes=user_pastes,
+                                    comments=user_comments)
         else:
             flash('In order to view your pastes you need to be logged in!')
             return redirect(url_for('home'))
     else:
         return redirect(url_for('home'))
 
+
 @app.route('/archive')
 @app.route('/archive/<int:page>')
-def show_archive(page = 1):
-    paste = Paste.query.filter_by(anonymous=True).order_by('pub_date desc').paginate(page,25,False)
-    return render_template('archive.html',pastes = paste)
+def show_archive(page=1):
+    paste = Paste.query.filter_by(anonymous=True).order_by(
+        'pub_date desc').paginate(page, 25, False)
+    return render_template('archive.html', pastes=paste)
+
 
 @app.route('/about')
 def about_page():
     return render_template('about.html')
+
 
 @app.route('/sitemap.xml')
 def show_sitemap():
     pastes = Paste.query.filter_by(anonymous=True)
     pages = []
     url_root = request.url_root[:-1]
-    ten_days_ago= datetime.utcnow().strftime('%Y-%m-%d %H:%M')
+    ten_days_ago = datetime.utcnow().strftime('%Y-%m-%d %H:%M')
     for rule in app.url_map.iter_rules():
-          if "GET" in rule.methods and len(rule.arguments)==0:
-              pages.append(
-                           [url_root+rule.rule,ten_days_ago]
-                           )
+        if "GET" in rule.methods and len(rule.arguments) == 0:
+            pages.append(
+                [url_root + rule.rule, ten_days_ago]
+            )
     for paste in pastes:
-        url = url_for('show_paste',paste_id=paste.uuid_id,_external=True)
+        url = url_for('show_paste', paste_id=paste.uuid_id, _external=True)
         modified_time = paste.pub_date.strftime('%Y-%m-%d %H:%M')
-        pages.append([url,modified_time]) 
+        pages.append([url, modified_time])
 
     sitemap_xml = render_template('sitemap.xml', pages=pages)
-    response= make_response(sitemap_xml)
-    response.headers["Content-Type"] = "application/xml"   
+    response = make_response(sitemap_xml)
+    response.headers["Content-Type"] = "application/xml"
     return response
+
 
 @app.route('/robots.txt')
 def show_robotstxt():
-    response= make_response(render_template('robots.txt'))
+    response = make_response(render_template('robots.txt'))
     response.headers["content-type"] = "text/plain"
     return response
+
 
 @app.route('/feedback')
 def feedback():
     return render_template('feedback.html')
-#---------------------------------------------Authentication------------------------------------#                                    
+
 
 @app.route('/login', methods=('GET', 'POST'))
 def login():
@@ -276,6 +300,7 @@ def login():
             return render_template('login.html', form=form)
     return render_template('login.html', form=form)
 
+
 @app.route('/signup', methods=('GET', 'POST'))
 def signup():
     if session['user_email']:
@@ -287,7 +312,7 @@ def signup():
         user_name = User.query.filter_by(name=form.name.data).first()
         user_email = User.query.filter_by(email=form.email.data).first()
         if user_name is None and user_email is None:
-            user = User(form.name.data,form.email.data,form.password.data)
+            user = User(form.name.data, form.email.data, form.password.data)
             db.session.add(user)
             db.session.commit()
             session['user_email'] = form.email.data
@@ -295,12 +320,13 @@ def signup():
             flash('Thanks for registering. You are now logged in!')
             return redirect(url_for('home'))
         elif user_name is None:
-            flash("email already exists. Choose another one!",'error')
+            flash("email already exists. Choose another one!", 'error')
             render_template('signup.html', form=form)
         else:
-            flash("username already exists. Choose another one!",'error')
+            flash("username already exists. Choose another one!", 'error')
             render_template('signup.html', form=form)
     return render_template('signup.html', form=form)
+
 
 @app.route('/logout', methods=('GET', 'POST'))
 def logout():
@@ -309,9 +335,6 @@ def logout():
     flash("You were successfully logged out")
     return redirect(request.referrer or url_for('home'))
 
-#-----------------------------------------------------------------------------------------------#
-#                                                Main run                                       #
-#-----------------------------------------------------------------------------------------------#
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 8080))
--- forms.py
+++ forms.py
@@ -1,16 +1,18 @@
 from flask_wtf import Form
-from wtforms import TextField , PasswordField
-from wtforms.validators import DataRequired , Email , Length, EqualTo , Required
+from wtforms import TextField, PasswordField
+from wtforms.validators import DataRequired, Email, Length, EqualTo, Required
+
 
 class LoginForm(Form):
-    email = TextField('email', validators=[DataRequired(),Email()])
+    email = TextField('email', validators=[DataRequired(), Email()])
     password = TextField('password', validators=[DataRequired()])
 
+
 class SignupForm(Form):
-    name = TextField('Name', validators=[DataRequired(),Length(min=4, max=25)])
+    name = TextField('Name', validators=[DataRequired(), Length(min=4, max=25)])
     password = PasswordField('Password', [
         Required(),
         EqualTo('confirm', message='Passwords must match')
     ])
     confirm = PasswordField('Repeat Password')
-    email = TextField('Email', validators=[DataRequired(),Length(min=5, max=35),Email()])
+    email = TextField('Email', validators=[DataRequired(), Length(min=5, max=35), Email()])
